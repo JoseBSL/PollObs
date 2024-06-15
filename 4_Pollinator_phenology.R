81ad8546641b4717699ae31c8010e6b0586e9735
JoseBSL <barragansljose@gmail.comcd>
2024-06-15 20:44:48 +0200

    edits

--- 4_Pollinator_phenology.R
+++ 4_Pollinator_phenology.R
@@ -8,8 +8,10 @@
 
 #Even using the three gardens does not seem enough for an accurate phenology
 #We are going to use GBIF occurrences from that year to complement our records
-#Yet not decided if do it at Country level, given the likely lack of records for some species
-#Or focus only on records close to Jena-Halle and Leipzig
+#At the moment we are going to download occurrences for Germany, Austria, Poland and Czechia
+#Then we will extract point within a radius with centroid between the three cities
+
+
 
 #Load libraries
 library(dplyr) #To manipulate data
@@ -85,3 +87,57 @@ scale_y_continuous(expand = c(0,0), breaks = c(NULL), labels = c(NULL))
 
 
 
+
+
+#In this script we download data from GBIF----
+#The download is done with the help of rgbif and the function occ_download
+#Quite fast but we need a gbif account!
+#To reproduce our analysis, 
+#Skip this part and use directly the downloaded data from the DOI
+#https://doi.org/10.15468/dl.5s5kuf
+
+#Load libraries
+library(stringr) 
+library(dplyr) 
+library(rgbif) 
+library(sp) 
+library(rworldmap) 
+library(sf) 
+library(openssl)
+library(usethis) 
+
+#Here we need to add our credentials to download the data
+usethis::edit_r_environ()
+
+
+########################---
+#Download data from GBIF----
+########################---
+species = poll_phen %>% distinct(Pollinator) %>%  pull()
+
+#First create species key (some long numbers that are needed to download the data) 
+#For loop to do it for all species
+i <- NULL
+gbif_id <- list()
+
+for(i in species){
+    
+    j <- gsub(" ", "_", i)
+    gbif_id[[j]] <- name_backbone(name=i, rank = "species")$usageKey
+}
+
+#convert list to dataframe
+gbif_id <- data.frame(unlist(gbif_id))
+#rename col
+colnames(gbif_id) <- "key_number"
+
+#Countries of interest with ISO codes
+#Include Germany, Austria, Czechia, Poland
+countries <- c("GE", "AT", "CZ", "PL", "BE", "NL", "DK") 
+
+#Download data
+occ_download(pred_in("taxonKey",c(gbif_id$key_number)), pred_gte("year", 2003), pred_in("country", c("GE")))
+
+#The data is downloaded through my gbif account
+#Here is the final data: https://doi.org/10.15468/dl.5s5kuf
+#Last accessed 05/12/2022 
