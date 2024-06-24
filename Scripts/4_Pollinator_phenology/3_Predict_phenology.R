30cc8e0515fa42b99466c6ffc462608924508ef2
Jose B. Lanuza <barragansljose@gmail.com>
2024-06-24 20:42:03 +0200

    keep working with this code...
    Getting there

--- Scripts/4_Pollinator_phenology/3_Predict_phenology.R
+++ Scripts/4_Pollinator_phenology/3_Predict_phenology.R
@@ -32,21 +32,18 @@ pull(Species)
 oc1 = oc1 %>%
 arrange(match(Species, spp_order))
 
-#Start with the 1st species
-spp_order[1]
-
 #Create general function 
 #This function runs a GAM and plots 
 #the probabilities relative to the maximum abundance
 #Define the function
 create_prediction <- function(species) {
+    tryCatch({
+
   #Filter data for the specified species
   sp_data = oc1 %>% filter(Species == species)
   #Fit the GAM model
-  sp_model = gam(n_individuals ~ s(Doy, k = 5),
-                   gamma = 0.8,
-                   family = poisson,
-                   data = sp_data)
+  sp_model = gam(n_individuals ~ s(Doy, k = 20),
+               family = nb(link = "log"), data = sp_data, method = "REML")
   #Create a new data frame with unique Doy values
   unique_dates = tibble(Doy = seq(from = 1, to = 365, by = 1))
   #Make predictions using the new data frame
@@ -66,72 +63,78 @@ create_prediction <- function(species) {
          x = "Day of Year (Doy)",
          y = "Predicted Probability") +
     theme_minimal()
-  
-  # Save the plot to a file
-  plot_filename <- paste0("plot_", species, ".png")
-  ggsave(filename = plot_filename, plot = plot, width = 8, height = 6)  
-
-  print(plot) #Ensure the plot is printed
-  #Return the data frame with predictions
 # Return a tibble with the predictions and the plot
-  return(tibble(Species = species, Predictions = list(sp1_new), Plot = list(plot)))
-  }
+  return(tibble(Species = species, Predictions = list(unique_dates), Plot = list(plot)))
+  }, error = function(e) {
+    message(paste("Error for species:", species, ":", e$message))
+    return(NULL)
+  })}
 
 #Run it species by species
 # Assuming oc1 and spp_order are already defined
 predictions_list <- list()
 
-for (i in 1:min(50, length(spp_order))) {
+for (i in 1:length(spp_order)) {
   species <- spp_order[i]
   predictions <- create_prediction(species = species)
   predictions_list[[species]] <- predictions
   print(paste("Completed predictions for", species))
 }
 
-# Combine all predictions into a single data frame if needed
+#Combine all predictions into a single data frame if needed
 all_predictions <- bind_rows(predictions_list, .id = "Species")
 
-#Check plot by plot
-all_predictions$Plot[1] #Weird pattern
-all_predictions$Plot[2] #Weird pattern
-all_predictions$Plot[3] #Weird pattern
-all_predictions$Plot[4] #Weird pattern
-all_predictions$Plot[5] #Weird pattern
-all_predictions$Plot[6] #Weird pattern
-all_predictions$Plot[7] #Weird pattern
-all_predictions$Plot[8] #Weird pattern
-all_predictions$Plot[9] #Weird pattern
-all_predictions$Plot[10] #Weird pattern
-all_predictions$Plot[11] #Weird pattern
-all_predictions$Plot[12] #Weird pattern
-all_predictions$Plot[13] #Weird pattern
-all_predictions$Plot[14] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[16] #Weird pattern
-all_predictions$Plot[17] #Weird pattern
-all_predictions$Plot[18] #Weird pattern
-all_predictions$Plot[19] #Weird pattern
-all_predictions$Plot[20] #Weird pattern
-all_predictions$Plot[21] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
+#Now save all plots in a folder ordered by number so I can see if it worked
+#For some species is not working, let's ensure that we have a plot for all species
+
+sp_tibble = tibble(Species = spp_order)
+all_predictions1 = left_join(sp_tibble, all_predictions) 
+
+for (i in length(all_predictions1$Species)) {
+  species <- all_predictions1$Species[i]
+  
+  # Extract the plot from the tibble using double brackets [[i]]
+  plot <- all_predictions1$Plot[[i]]
+  
+  # Check if plot is not NULL before saving
+  if (!is.null(plot)) {
+    # Construct the filename with the species index and name
+    plot_filename <- paste0("Data/Pollinator_phenology/", i, "_", stringr::str_replace(species, " ","_"), ".png")
+    
+    # Save the plot to a file
+    ggsave(filename = plot_filename, plot = plot, width = 8, height = 6)
+    
+    print(paste("Saved plot for", species))
+  } else {
+    print(paste("Plot for", species, "is NULL, skipping saving."))
+  }
+}
 
 
+#Check fit of specific gam plots---
+sp_data = oc1 %>% filter(Species == spp_order[8])
+sp_model = gam(n_individuals ~ s(Doy, k = 15),
+               family = nb(link = "log"), data = sp_data, method = "REML")
+qq.gam(sp_model, main = "QQ plot of residuals")
+#Looks good
+#Create a new data frame with unique Doy values
+unique_dates = tibble(Doy = seq(from = 1, to = 365, by = 1))
+#Make predictions using the new data frame
+predicted_values = predict(sp_model, unique_dates, type = "response")
+#Assign the predicted values to the new data frame
+unique_dates$Abundances = round(predicted_values)
+#Normalize the predicted values to convert to probabilities
+max_abundance = max(unique_dates$Abundances)
+unique_dates = unique_dates %>%
+mutate(Probability = Abundances / max_abundance)
+#Add the species name to the data frame
+unique_dates$Species = spp_order[8]
+#Plot the predicted probabilities
+ggplot(unique_dates, aes(x = Doy, y = Probability)) +
+  geom_line(color = "blue") +
+    labs(title = paste("Predicted Probabilities for", spp_order[], "Over Day of Year (Doy)"),
+         x = "Day of Year (Doy)",
+         y = "Predicted Probability") +
+    theme_minimal()
 
-all_predictions$Plot[4] #Weird pattern
-all_predictions$Plot[6] #Cool example of a narrow phenology
-all_predictions$Plot[10] #Cool example of a narrow phenology
-all_predictions$Plot[13] #Weird pattern
-all_predictions$Plot[14] #Weird pattern
-all_predictions$Plot[15] #Weird pattern
-all_predictions$Plot[17] #Weird pattern
+
