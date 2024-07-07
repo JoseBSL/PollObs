ba4df9a2e497425d59f2e9015bd96e3338b80e7c
Jose B. Lanuza <barragansljose@gmail.com>
2024-07-07 23:40:12 +0200

    pollinator phenology, almost ready

--- Scripts/4_Pollinator_phenology/checks.R
+++ Scripts/4_Pollinator_phenology/checks.R
@@ -1,10 +1,12 @@
 
 
 # Check fit of some models
-sp_data = oc1 %>% filter(Species == spp_order[4])
-sp_model = gam(n_individuals ~ s(Doy, k = 12, m=2),
+species =  "Andrena hattorfiana"
+sp_data = oc2 %>% filter(Species == species)
+sp_model = gam(n_individuals ~ s(Doy, k = 8, m=2),
                family = nb(link = "log"), data = sp_data, method = "REML")
 qq.gam(sp_model, main = "QQ plot of residuals")
+sp_data1 = oc3 %>% filter(Species == species)
 
 #Looks good
 #Create a new data frame with unique Doy values
@@ -18,13 +20,15 @@ max_abundance = max(unique_dates$Abundances)
 unique_dates = unique_dates %>%
 mutate(Probability = Abundances / max_abundance)
 #Add the species name to the data frame
-unique_dates$Species = spp_order[4]
+unique_dates$Species = species
 #Plot the predicted probabilities
 ggplot(unique_dates, aes(x = Doy, y = Probability)) +
   geom_line(color = "blue") +
-    labs(title = paste("Predicted Probabilities for", spp_order[4], "Over Day of Year (Doy)"),
+    labs(title = paste("Predicted Probabilities for", species, "Over Day of Year (Doy)"),
          x = "Day of Year (Doy)",
          y = "Predicted Probability") +
-    theme_minimal()
+  theme_minimal() +
+  geom_point(data = sp_data, aes(x = Doy, y = n_individuals/(max(n_individuals)))) +
+  geom_vline(data = sp_data1, aes(xintercept = Doy), linetype = "dashed") 
 
 
