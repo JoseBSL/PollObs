be91ba4ceb757af04287fecbec4fb72222cd50a5
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2024-07-11 21:31:08 +0200

    Create Percentage_visited_plant_species_by_pollinator.R

---
+++ Scripts/Percentage_visited_plant_species_by_pollinator.R
@@ -0,0 +1,65 @@
+
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(bipartite)
+
+# ONLY FOCALS + RD OBSERVATIONS
+
+#Load data
+raw_data = readRDS("Data/Working_files/interaction_data.rds")
+interaction_data <- raw_data  %>% filter(!is.na(Interactions),
+                                         !is.na(Floral_abundance),
+                                         Pollinator != "None") %>% 
+  select(Botanical_garden, Plant_accepted_name, 
+         Pollinator_accepted_name, Date_time, 
+         Interactions, Floral_abundance) %>% 
+  rename(Plant = Plant_accepted_name, Pollinator = Pollinator_accepted_name) %>% 
+  mutate(Date = as.Date(Date_time)) %>% 
+  mutate(Week = lubridate::week(Date)) %>% 
+  select(-Date_time, -Date) %>% filter(!is.na(Pollinator)) %>% ungroup()
+
+############################################################################
+# Weekly estimations
+############################################################################
+
+total_plant_Sp_week <- interaction_data %>%
+  dplyr::select(Botanical_garden, Plant, Week) %>% unique() %>%
+  group_by(Botanical_garden, Week) %>% count() %>%
+  rename(Total_plant_richness = n) %>% ungroup()
+
+total_visited_plant_Sp_by_poll_week <- interaction_data %>%
+  dplyr::select(Botanical_garden, Plant, Pollinator, Week) %>% unique() %>%
+  group_by(Botanical_garden, Pollinator, Week) %>% count() %>%
+  rename(Total_visited_plant_richness = n) %>% ungroup()
+
+data_visited_plant_Sp_by_poll_week <- total_visited_plant_Sp_by_poll_week %>%
+  left_join(total_plant_Sp_week, 
+            by = c("Botanical_garden", "Week")) %>%
+  mutate(percentage_vivited_plant_sp = Total_visited_plant_richness/Total_plant_richness)
+
+############################################################################
+# Flowering season estimation
+############################################################################
+
+total_plant_Sp_season <- interaction_data %>%
+  dplyr::select(Botanical_garden, Plant) %>% unique() %>%
+  group_by(Botanical_garden) %>% count() %>%
+  rename(Total_plant_richness = n) %>% ungroup()
+
+total_visited_plant_Sp_by_poll_season <- interaction_data %>%
+  dplyr::select(Botanical_garden, Plant, Pollinator) %>% unique() %>%
+  group_by(Botanical_garden, Pollinator) %>% count() %>%
+  rename(Total_visited_plant_richness = n) %>% ungroup()
+
+data_visited_plant_Sp_by_poll_season <- total_visited_plant_Sp_by_poll_season %>%
+  left_join(total_plant_Sp_season, 
+            by = c("Botanical_garden")) %>%
+  mutate(percentage_vivited_plant_sp = Total_visited_plant_richness/Total_plant_richness)
+
+############################################################################
+# save results
+############################################################################
+
+readr::write_csv(data_visited_plant_Sp_by_poll_week,"Data/Working_files/data_visited_plant_Sp_by_poll_week.csv")
+readr::write_csv(data_visited_plant_Sp_by_poll_season,"Data/Working_files/data_visited_plant_Sp_by_poll_season.csv")
