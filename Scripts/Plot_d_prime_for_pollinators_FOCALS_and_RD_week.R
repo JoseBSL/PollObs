a3975f1cc1cf87a050527c6cc3b1f1ed26ce5e06
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2024-07-11 21:31:13 +0200

    Create Plot_d_prime_for_pollinators_FOCALS_and_RD_week.R

---
+++ Scripts/Plot_d_prime_for_pollinators_FOCALS_and_RD_week.R
@@ -0,0 +1,59 @@
+
+library(dplyr)
+library(tidyr)
+library(readr)
+library(ggplot2)
+library(patchwork)
+
+d_prime_poll_interactions <- readr::read_csv("Data/Working_files/average_d_prime_by_week_corrected_by_int.csv") %>%
+  dplyr::select(Botanical_garden, Week, av_dprime_poll, 
+                av_dprime_poll_upper, av_dprime_poll_lower) %>%
+  rename(av_dprime_poll_int = av_dprime_poll, 
+         av_dprime_poll_int_upper = av_dprime_poll_upper, 
+         av_dprime_poll_int_lower = av_dprime_poll_lower)
+
+d_prime_poll_abundance <- readr::read_csv("Data/Working_files/average_d_prime_by_week_corrected_by_ab.csv") %>%
+  dplyr::select(Botanical_garden, Week, av_dprime_poll, 
+                av_dprime_poll_upper, av_dprime_poll_lower) %>%
+  rename(av_dprime_poll_ab = av_dprime_poll, 
+         av_dprime_poll_ab_upper = av_dprime_poll_upper, 
+         av_dprime_poll_ab_lower = av_dprime_poll_lower)
+
+
+average_specialization <- d_prime_poll_interactions %>%
+  left_join(d_prime_poll_abundance, by = c("Botanical_garden","Week"))
+
+plot_av_dprime_poll_interactions <- ggplot(average_specialization, aes(x=Week,y=av_dprime_poll_int, color = Botanical_garden))+
+  geom_point(size=3)+
+  geom_line(linewidth=1.5)+
+  geom_ribbon(aes(ymax = av_dprime_poll_int_upper,
+                  ymin = av_dprime_poll_int_lower, 
+                  fill = Botanical_garden),alpha=.2, colour = NA)+
+  ylim(0,1)+
+  labs(x="Week number", y= "Weighted mean d prime index\nfor pollinators", title = "Only interactions\nare considered", color = NULL)+
+  theme_bw()+
+  theme(legend.position = "bottom", legend.title = element_blank())+
+  theme(legend.text = element_text(size = 18),
+        axis.text=element_text(size=16),
+        axis.title=element_text(size=16,face="bold"),
+        plot.title=element_text(size=16,face="bold"),
+        strip.text = element_text(size = 18))+ guides(fill="none")
+
+plot_av_dprime_poll_abundance <- ggplot(average_specialization, aes(x=Week,y=av_dprime_poll_ab, color = Botanical_garden))+
+  geom_point(size=3)+
+  geom_line(linewidth=1.5)+
+  geom_ribbon(aes(ymax = av_dprime_poll_ab_upper,
+                  ymin = av_dprime_poll_ab_lower, 
+                  fill = Botanical_garden),alpha=.2, colour = NA)+
+  ylim(0,1)+
+  labs(x="Week number", y= "Weighted mean d prime index\nfor pollinators", title = "Flower abundance\nis also considered", color = NULL)+
+  theme_bw()+
+  theme(legend.position = "bottom", legend.title = element_blank())+
+  theme(legend.text = element_text(size = 18),
+        axis.text=element_text(size=16),
+        axis.title=element_text(size=16,face="bold"),
+        plot.title=element_text(size=16,face="bold"),
+        strip.text = element_text(size = 18))+ guides(fill="none")
+
+
+(plot_av_dprime_poll_interactions|plot_av_dprime_poll_abundance) + plot_layout(guides = "collect") & theme(legend.position = 'bottom')
