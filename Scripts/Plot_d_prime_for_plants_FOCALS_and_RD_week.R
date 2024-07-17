ef21d0af986c00515d596d8653810fcfb9d2503c
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2024-07-17 17:08:31 +0200

    Create Plot_d_prime_for_plants_FOCALS_and_RD_week.R

---
+++ Scripts/Plot_d_prime_for_plants_FOCALS_and_RD_week.R
@@ -0,0 +1,81 @@
+
+library(dplyr)
+library(tidyr)
+library(readr)
+library(ggplot2)
+library(patchwork)
+
+d_prime_plant_interactions <- readr::read_csv("Data/Working_files/average_d_prime_by_week_corrected_by_int.csv") %>%
+  dplyr::select(Botanical_garden, Week, av_dprime_poll, 
+                av_dprime_plant_upper, av_dprime_plant_lower) %>%
+  rename(av_dprime_plant_int = av_dprime_poll, 
+         av_dprime_plant_int_upper = av_dprime_plant_upper, 
+         av_dprime_plant_int_lower = av_dprime_plant_lower)
+
+d_prime_plant_abundance <- readr::read_csv("Data/Working_files/average_d_prime_by_week_corrected_by_ab.csv") %>%
+  dplyr::select(Botanical_garden, Week, av_dprime_poll, 
+                av_dprime_plant_upper, av_dprime_plant_lower) %>%
+  rename(av_dprime_plant_ab = av_dprime_poll, 
+         av_dprime_plant_ab_upper = av_dprime_plant_upper, 
+         av_dprime_plant_ab_lower = av_dprime_plant_lower)
+
+
+average_specialization <- d_prime_plant_interactions %>%
+  left_join(d_prime_plant_abundance, by = c("Botanical_garden","Week"))
+
+average_specialization_SEASON_int <- readr::read_csv("Data/Working_files/average_d_prime_by_SEASON_corrected_by_int.csv")
+average_specialization_SEASON_ab <- readr::read_csv("Data/Working_files/average_d_prime_by_SEASON_corrected_by_ab.csv")
+
+
+plot_av_dprime_plant_interactions <- ggplot(average_specialization, aes(x=Week,y=av_dprime_plant_int, color = Botanical_garden))+
+  geom_point(size=3)+
+  geom_line(linewidth=1.5)+
+  geom_hline(yintercept = 
+               average_specialization_SEASON_int$av_dprime_poll[average_specialization_SEASON_int$Botanical_garden=="Jena"],
+             linewidth = 1.5, color = "#00BA38", linetype = "dashed")+
+  geom_hline(yintercept = 
+               average_specialization_SEASON_int$av_dprime_poll[average_specialization_SEASON_int$Botanical_garden=="Halle"],
+             linewidth = 1.5, color = "#F8766D", linetype = "dashed")+
+  geom_hline(yintercept = 
+               average_specialization_SEASON_int$av_dprime_poll[average_specialization_SEASON_int$Botanical_garden=="Leipzig"],
+             linewidth = 1.5, color = "#619CFF", linetype = "dashed")+
+  # geom_ribbon(aes(ymax = av_dprime_plant_int_upper,
+  #                 ymin = av_dprime_plant_int_lower, 
+  #                 fill = Botanical_garden),alpha=.2, colour = NA)+
+  ylim(0,1)+
+  labs(x="Week number", y= "Weighted mean d prime index\nfor plants", title = "Only interactions\nare considered", color = NULL)+
+  theme_bw()+
+  theme(legend.position = "bottom", legend.title = element_blank())+
+  theme(legend.text = element_text(size = 18),
+        axis.text=element_text(size=16),
+        axis.title=element_text(size=16,face="bold"),
+        plot.title=element_text(size=16,face="bold"),
+        strip.text = element_text(size = 18))+ guides(fill="none")
+
+plot_av_dprime_plant_abundance <- ggplot(average_specialization, aes(x=Week,y=av_dprime_plant_ab, color = Botanical_garden))+
+  geom_point(size=3)+
+  geom_line(linewidth=1.5)+
+  geom_hline(yintercept = 
+               average_specialization_SEASON_ab$av_dprime_poll[average_specialization_SEASON_ab$Botanical_garden=="Jena"],
+             linewidth = 1.5, color = "#00BA38", linetype = "dashed")+
+  geom_hline(yintercept = 
+               average_specialization_SEASON_ab$av_dprime_poll[average_specialization_SEASON_ab$Botanical_garden=="Halle"],
+             linewidth = 1.5, color = "#F8766D", linetype = "dashed")+
+  geom_hline(yintercept = 
+               average_specialization_SEASON_ab$av_dprime_poll[average_specialization_SEASON_ab$Botanical_garden=="Leipzig"],
+             linewidth = 1.5, color = "#619CFF", linetype = "dotted")+
+  # geom_ribbon(aes(ymax = av_dprime_plant_ab_upper,
+  #                 ymin = av_dprime_plant_ab_lower, 
+  #                 fill = Botanical_garden),alpha=.2, colour = NA)+
+  ylim(0,1)+
+  labs(x="Week number", y= "Weighted mean d prime index\nfor plants", title = "Pollinator abundance\nis also considered", color = NULL)+
+  theme_bw()+
+  theme(legend.position = "bottom", legend.title = element_blank())+
+  theme(legend.text = element_text(size = 18),
+        axis.text=element_text(size=16),
+        axis.title=element_text(size=16,face="bold"),
+        plot.title=element_text(size=16,face="bold"),
+        strip.text = element_text(size = 18))+ guides(fill="none")
+
+
+(plot_av_dprime_plant_interactions|plot_av_dprime_plant_abundance) + plot_layout(guides = "collect") & theme(legend.position = 'bottom')
