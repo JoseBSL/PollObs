8217b641ebb9f5e41d32242d92105407d3fb983e
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2024-07-18 06:47:50 +0200

    Update Estimate z_scores_4_neutral_interaction_by_GENUS.R

--- Scripts/Estimate z_scores_4_neutral_interaction_by_GENUS.R
+++ Scripts/Estimate z_scores_4_neutral_interaction_by_GENUS.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(tidyr)
 library(lubridate)
 library(readr)
+library(ggplot2)
 
 #Load data
 raw_data = readRDS("Data/Working_files/interaction_data.rds")
@@ -49,6 +50,34 @@ z_sc_data_interactions_genus_aux[is.na(z_sc_data_interactions_genus_aux)] <- 0
 z_sc_data_interactions_genus_final <- z_sc_data_interactions_genus_aux %>%
   mutate(z_score = (Total_interactions-Mean_total_interactions)/SD_total_interactions)
 
+z_sc_data_interactions_genus_final$Type <- "Normal"
+z_sc_data_interactions_genus_final$Type[z_sc_data_interactions_genus_final$z_score >= 1.96] <- "Over-represented"
+z_sc_data_interactions_genus_final$Type[z_sc_data_interactions_genus_final$z_score <= -1.96] <- "Under-represented"
+
 z_sc_data_interactions_genus_final_filtered <- z_sc_data_interactions_genus_final %>%
   filter(z_score >= 1.96 | z_score <= -1.96)
+  
+
+significant_pairs_week <- z_sc_data_interactions_genus_final_filtered %>% 
+  mutate(Pair = paste0(Plant_genus,"_",Pollinator_genus)) %>% 
+  group_by(Botanical_garden,Pair,Type) %>% count() %>% 
+  rename(Weeks_significant = n) 
+
+
+ggplot(significant_pairs_week,
+       aes(x=Weeks_significant, fill = Type))+
+  geom_histogram(position = "stack")+
+  facet_wrap(~Botanical_garden)+
+  labs(x="Total number of weeks in the study where a pairwise\ninteraction was found to be significant", 
+       y = "Number of significant pairwise interactions\nduring the sampling period", fill=NULL)+
+  theme_bw()+
+  theme(legend.position = "bottom")+
+  theme(legend.text = element_text(size = 16),
+        axis.text=element_text(size=17),
+        axis.title=element_text(size=16,face="bold"),
+        plot.title=element_text(size=16,face="bold"),
+        strip.text = element_text(size = 18))
+
+
+significant_pairs_week %>% filter(Weeks_significant>= 6)
   
