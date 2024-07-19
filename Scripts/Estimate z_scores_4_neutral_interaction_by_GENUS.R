c64e931861a65830cad9ae9d90897862e1b947e2
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2024-07-19 12:13:14 +0200

    Update Estimate z_scores_4_neutral_interaction_by_GENUS.R

--- Scripts/Estimate z_scores_4_neutral_interaction_by_GENUS.R
+++ Scripts/Estimate z_scores_4_neutral_interaction_by_GENUS.R
@@ -36,7 +36,7 @@ Total_number_samples_week <- 1000
 data_neutral_interactions_genus <-  
   readr::read_csv(paste0("Data/Working_files/neutral_weekly_interactions_by_GENUS_",
                  Total_number_samples_week,
-                 "_samples.csv"))%>%
+                 "_samples.csv")) %>%
   group_by(Botanical_garden, Week, Plant_genus, Pollinator_genus) %>%
   summarise(
     Mean_total_interactions = mean(Total_interactions),
@@ -131,7 +131,7 @@ z_sc_data_interactions_genus_final$Type <- factor(z_sc_data_interactions_genus_f
 ggplot(z_sc_data_interactions_genus_final %>% filter(!is.infinite(z_score)), 
        aes(x=z_score, y = Total_interactions, fill= Type))+
          geom_point(size=3.2, alpha= 0.35, shape=21, color="gray", stroke=0.5)+
-  scale_x_continuous(trans=scales::pseudo_log_trans(base = 10), breaks=c(-100,-10,-1.96,1.96, 10, 100))+
+  # scale_x_continuous(trans=scales::pseudo_log_trans(base = 10), breaks=c(-100,-10,-1.96,1.96, 10, 100))+
   scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
   labs(y="Total number of observed interactions by genus", 
@@ -142,4 +142,151 @@ theme_bw()+
   theme(legend.text = element_text(size = 17),
         axis.text=element_text(size=17),
         axis.title=element_text(size=16,face="bold"))
-       
+
+z_sc_data_interactions_genus_final %>% filter(Type=="Non-significant") %>%
+  arrange(desc(Total_interactions))
+
+
+
+#Load data
+raw_data = readRDS("Data/Working_files/interaction_data.rds")
+
+
+#Prepare plant and pollinator data by genus
+data_floral_ab_genus <- raw_data  %>% filter(!is.na(Interactions),
+                                             !is.na(Floral_abundance),
+                                             Pollinator != "None") %>% 
+  mutate(Individual = paste0(Sampling,Random_census_stop)) %>% 
+  dplyr::select(Botanical_garden, Plant_genus, Plant_accepted_name, 
+                Date_time, Floral_abundance, Individual) %>% 
+  mutate(Plant = Plant_accepted_name, Date = as.Date(Date_time)) %>% 
+  mutate(Week = lubridate::week(Date)) %>% 
+  dplyr::select(-Date_time, -Date) %>% ungroup() %>% 
+  dplyr::select(Botanical_garden,Plant_genus, Plant, Week,
+                Floral_abundance, Individual) %>% unique() %>%
+  group_by(Botanical_garden, Plant_genus, Week) %>%
+  summarise(
+    Total_floral_abundance_genus = sum(Floral_abundance),
+  ) %>% ungroup() %>% arrange(Week)
+
+z_sc_data_interactions_genus_final_fl_ab <- z_sc_data_interactions_genus_final %>%
+  left_join(data_floral_ab_genus,
+            by = c("Botanical_garden", "Week", 
+                   "Plant_genus"))
+
+ggplot(z_sc_data_interactions_genus_final_fl_ab %>% filter(!is.infinite(z_score)), 
+       aes(x=z_score, y = Total_interactions, fill= log10(Total_floral_abundance_genus)))+
+  geom_point(size=3.2, alpha= 0.35, shape=21, color="gray", stroke=0.5)+
+  scale_x_continuous(trans=scales::pseudo_log_trans(base = 10), breaks=c(-100,-10,-1.96,1.96, 10, 100))+
+  scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  labs(y="Total number of observed interactions by genus", 
+       x = "Estimated z-score", fill="log10(floral ab.)")+
+  scale_fill_viridis() +
+  theme_bw()+
+  theme(legend.text = element_text(size = 17),
+        axis.text=element_text(size=17),
+        axis.title=element_text(size=16,face="bold"))
+
+
+ggplot(z_sc_data_interactions_genus_final_fl_ab %>% filter(!is.infinite(z_score)), 
+       aes(x=z_score, y = Total_floral_abundance_genus, fill = log10(Total_interactions)))+
+  geom_point(size=3.2, alpha= 0.35, shape=21, color="gray", stroke=0.5)+
+  scale_x_continuous(trans=scales::pseudo_log_trans(base = 10), breaks=c(-100,-10,-1.96,1.96, 10, 100))+
+  scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  labs(y="Foral abundance by genus", 
+       x = "Estimated z-score", fill="log10(Total interactions)")+
+  scale_fill_viridis() +
+  theme_bw()+
+  theme(legend.text = element_text(size = 17),
+        axis.text=element_text(size=17),
+        axis.title=element_text(size=16,face="bold"))
+
+ggplot(z_sc_data_interactions_genus_final_fl_ab %>% filter(!is.infinite(z_score)), 
+       aes(x=Type, y = Total_interactions))+
+  geom_boxplot()+
+  scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  labs(y="Total number of observed interactions by genus", 
+       x = NULL, fill=NULL)+
+  scale_fill_viridis() +
+  theme_bw()+
+  theme(legend.position = "bottom")+
+  theme(legend.text = element_text(size = 17),
+        axis.text=element_text(size=17),
+        axis.title=element_text(size=16,face="bold"))
+
+
+ggplot(z_sc_data_interactions_genus_final_fl_ab %>% filter(!is.infinite(z_score)), 
+       aes(x=Type, y = Total_floral_abundance_genus))+
+  geom_boxplot()+
+  # scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+  #               labels = trans_format("log10", math_format(10^.x))) +
+  labs(y="Floral abundance by genus", 
+       x = NULL, fill=NULL)+
+  scale_fill_viridis() +
+  theme_bw()+
+  theme(legend.position = "bottom")+
+  theme(legend.text = element_text(size = 17),
+        axis.text=element_text(size=17),
+        axis.title=element_text(size=16,face="bold"))
+
+####################################################################
+# MODELING Z-SCORE
+####################################################################
+
+library(glmmTMB)
+
+data_model <- z_sc_data_interactions_genus_final_fl_ab %>% filter(!is.infinite(z_score), !is.nan(z_score)) %>% 
+  mutate(Pair=paste0(Plant_genus,"_",Pollinator_genus),
+         Log_floral_abundance =log(Total_floral_abundance_genus),
+         Log_total_interactions =log(Total_interactions))
+
+min_zscore <- min(data_model$z_score)
+
+data_model$log_new_z_score <- log(data_model$z_score+abs(min_zscore)+1)
+data_model$new_z_score <- data_model$z_score+abs(min_zscore)+1
+
+model <- glmmTMB(abs(z_score) ~  Botanical_garden + scale(Log_floral_abundance)  +
+                            (1|Week/Pair),
+                 family = Gamma(link = "log"),
+                          data = data_model)
+summary(model)
+performance::check_collinearity(model)
+
+library(DHARMa)
+simulationOutput <- DHARMa::simulateResiduals(fittedModel = model)
+plot(simulationOutput)
+testDispersion(simulationOutput)
+testZeroInflation(simulationOutput)
+
+library(ggeffects)
+effects_model <- ggpredict(model, terms = c("Botanical_garden", "Log_floral_abundance"))
+
+plot(effects_model)
+
+mean_Log_floral_abundance <- mean(data_model$Log_floral_abundance)
+sd_Log_floral_abundance <- sd(data_model$Log_floral_abundance)
+
+
+effects_model$group2 = signif(exp(as.numeric(effects_model$group)*sd_Log_floral_abundance+mean_Log_floral_abundance), 1)
+
+
+ggplot(effects_model, aes(x = x, y = predicted, color = as.factor(group2))) +
+  geom_point(size = 2, position=position_dodge(width=0.5)) +
+  geom_hline(yintercept = 1.96, linewidth= 1.3, linetype = "dashed")+
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high, color = as.factor(group2),width=.1),
+                position=position_dodge(width=0.5)) +
+  labs(x = NULL,
+       y = "abs(z score)",
+       color =  "Flower abundance by genus")+
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme(legend.position = "bottom")+
+  theme(legend.text = element_text(size = 18),
+        legend.title=element_text(size=18,face="bold"),
+        axis.text=element_text(size=16),
+        axis.title=element_text(size=18,face="bold"),
+        plot.title=element_text(size=19,face="bold"),
+        strip.text = element_text(size = 18))
