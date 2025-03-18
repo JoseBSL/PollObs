9552535bd04a0b058c2df8669067b6fdeba332fe
Jose B. Lanuza <joseb.lanuza@iMac-de-Jose.local>
2025-03-18 17:46:38 +0100

    edits

---
+++ Scripts/2_1_Plant_traits/2_PhenObs_trait_spectrum_ALL.R
@@ -0,0 +1,274 @@
+#Script to prepare all plant traits from PhenObs
+#1)Prepare traits used in Lanuza 2023
+#Those are: 
+#Selfing
+#Plant height
+#Flower size
+#Flower number
+#Style length
+#Ovule number
+
+#2)Select other traits of interest and format accordingly
+#Flowering length
+#Flower lifespan
+
+#Load libraries
+library(dplyr)
+library(readr)
+library(conflicted)
+library(lubridate)
+
+conflict_prefer("select", # the function
+                "dplyr")
+#Load morphometrics and select/rename column of interest
+morphometrics = read_csv("Data/Trait_data/Raw/ReproductiveTraits_Morphometrics.csv")
+colnames(morphometrics)
+
+#1)Prepare traits used in Lanuza 2023
+lanuza_2023_traits = morphometrics %>% 
+ select(
+   Species,
+   Flower_number, 
+   Flower_width,
+   Style_length, 
+   `Ovule_number/flower`,
+   Plant_height_mm) %>% 
+ rename(Flowers_per_plant = Flower_number) %>% 
+ rename(Corolla_diameter_mean = Flower_width) %>% 
+ rename(Ovule_number = `Ovule_number/flower`) %>% 
+ rename(Plant_height_mean_m = Plant_height_mm) %>% 
+ mutate(Plant_height_mean_m = Plant_height_mean_m/1000) %>% 
+ mutate(Style_length = as.numeric(Style_length)) %>% 
+ group_by(Species) %>%
+ summarise(
+   Flowers_per_plant = mean(Flowers_per_plant, na.rm = TRUE),
+   Corolla_diameter_mean = mean(Corolla_diameter_mean, na.rm = TRUE),
+   Style_length = mean(Style_length, na.rm = TRUE),
+   Ovule_number = mean(Ovule_number, na.rm = TRUE),
+   Plant_height_mean_m = mean(Plant_height_mean_m, na.rm = TRUE)
+  )
+
+#Add now selfing that is missing here
+selfing = read_csv("Data/Trait_data/Processed/Selfing.csv")
+colnames(selfing)
+selfing = selfing %>% 
+  select(c(Species, Average_fruits)) %>% 
+  rename(Autonomous_selfing_level_fruit_set = Average_fruits)
+#Add selfing to morphometircs
+lanuza_2023_traits = left_join(selfing, lanuza_2023_traits)
+#Lanuza traits are ready, jump onto the next ones
+
+#For the phylo we need taxonomic info
+#Retrieve it quickly from GBIF
+plant_spp = lanuza_2023_traits %>% pull(Species)
+#Check for futher taxonomic info
+matched_gbif_plants = name_backbone_checklist(name = plant_spp)
+#Select columns of interest and rename accordingly
+matched_gbif_plants1 = matched_gbif_plants %>% 
+  dplyr::select(c(family, verbatim_name)) %>% 
+  rename(Family_all = family) %>% 
+  rename(Species_all = verbatim_name) %>% 
+  mutate(Genus_all = word(Species_all, 1))
+#Bind cols
+lanuza_2023_traits1 = bind_cols(lanuza_2023_traits, matched_gbif_plants1)
+
+#2)Select other traits of interest and format accordingly
+colnames(morphometrics)
+
+#Select cols of interest and convert to date format
+date_columns = c("Flowering_start", 
+                  "Flowering_end", 
+                  "Flower_life_start", 
+                  "Flower_life_end")
+
+flowering_dates = morphometrics %>% 
+  select(Species, 
+       Flowering_start, 
+       Flowering_end,
+       Flower_life_start,
+       Flower_life_end) %>% 
+  mutate(across(all_of(date_columns), ~ dmy(.)))
+
+#Check that it works
+str(flowering_dates)
+#Now substract dates to get number of flowering days and flower life span
+flowering_dates1 = flowering_dates %>% 
+  mutate(Flowering_length = as.numeric(Flowering_end - Flowering_start)) %>% 
+  mutate(Flower_lifespan = as.numeric(Flower_life_end - Flower_life_start)) %>% 
+  select(Species, 
+       Flowering_length,
+       Flower_lifespan) %>% 
+  group_by(Species) %>% 
+  summarise(Mean_flowering_length = mean(Flowering_length, na.rm = TRUE),
+          Mean_flower_lifespan = round(mean(Flower_lifespan, na.rm = TRUE)))
+
+
+#Bind both datasets so I can compute a PCA with all traits for now
+all_traits = left_join(lanuza_2023_traits1, flowering_dates1)
+
+# Define the columns to transform
+cols_to_transform = c("Autonomous_selfing_level_fruit_set", 
+                      "Flowers_per_plant",
+                      "Corolla_diameter_mean", 
+                      "Style_length", 
+                      "Ovule_number", 
+                      "Plant_height_mean_m", 
+                      "Mean_flowering_length",
+                      "Mean_flower_lifespan")
+
+#Conduct log transformation and scaling using mutate across selected columns
+final_d = all_traits %>%
+  mutate(across(all_of(cols_to_transform), ~ log(. + 1))) %>%
+  mutate(across(all_of(cols_to_transform), ~ scale(.) %>% as.vector())) %>% # Convert to vector to avoid grouping issues
+  dplyr::select(all_of(cols_to_transform))
+
+#4) Get phylo
+#calculate phylo 
+phylo <- as.data.frame(cbind(pollobs_data1$Family_all, pollobs_data1$Genus_all, pollobs_data1$Species_all))
+colnames(phylo) <-  c("family", "genus", "species")
+#Select unique cases
+#phylo_2 <- phylo[!duplicated(phylo$species),]
+phylo_2 <- tibble(phylo)
+#get phylo
+phylo_output <- get_tree(sp_list = phylo_2, taxon = "plant")
+str(phylo_output)
+#Convert phylogenetic tree into matrix
+A_5 <- vcv.phylo(phylo_output)
+#Standardize to max value 1
+A_5 <- A_5/max(A_5)
+#Unify column names; remove underscore and remove asterik
+rownames(A_5) <- gsub("\\*", "", rownames(A_5))
+colnames(A_5) <- gsub("\\*", "", colnames(A_5))
+colnames(A_5) <- gsub("_", " ", colnames(A_5))
+rownames(A_5) <- gsub("_", " ", rownames(A_5))
+#4) Calculate PPCA
+#Standardize species names
+rownames(final_d) = all_traits$Species_all
+rownames(final_d) = gsub(" ", "_", rownames(final_d))
+#Run PCA
+phyl_pca_forest <- phyl.pca(phylo_output, final_d,method="lambda",mode="cov")
+#Save output
+#saveRDS(phyl_pca_forest, "Data/Working_files/local_pca_phenobs_species_output.rds")
+#saveRDS(final_d, "Data/Working_files/local_pca_phenobs_species_data.rds")
+
+
+#CALL the output PC for simplicity
+PC <- phyl_pca_forest
+#CHECK CONTENT
+#EIGENVALUES
+PC$Eval
+#PC score (POINTS)
+PC$S
+#PC loadings (ARROWS)
+PC$L
+s = tibble(sp = rownames(PC$S),
+           PC1 = PC$S[,1],
+           PC2 = PC$S[,2])
+
+percentage = round(diag(PC$Eval) / sum(PC$Eval) * 100, 2) #calculate percentage
+
+########################################################################################################################################################
+#4) PLOT PPCA
+########################################################################################################################################################
+# Theme for publication
+theme_ms <- function(base_size=12, base_family="Helvetica") {
+  (theme_bw(base_size = base_size, base_family = base_family)+
+     theme(text=element_text(color="black"),
+           axis.title=element_text( size = rel(1.6)),
+           axis.text=element_text(size = rel(1.6), color = "black"),
+           legend.title=element_text(face="bold"),
+           legend.text=element_text(size = rel(1.1)),
+           legend.background=element_rect(fill="transparent"),
+           legend.key.size = unit(0.4, 'lines'),
+           panel.border=element_rect(color="black",size=1),
+           panel.grid.minor.x =element_blank(),
+           panel.grid.minor.y= element_blank(),
+           panel.grid.major= element_blank(),
+           plot.title = element_text(size = rel(2.5), face="bold")
+     ))
+}
+#####
+#MASTER FUNCTION TO PLOT 
+#####
+#seems massive but in reality is a scatterplot with arrows
+#it has inside how to compute kernel density but do not think I'll use it
+#point density seems to do the job
+
+
+# Load plots
+local_phenobs_pca <- function(PC, x = "PC1", y = "PC2") {
+  # PC being a prcomp object
+  data <- data.frame(PC$S)
+  plot <- ggplot(data, aes_string(x = x, y = y)) 
+  
+  dat <- data.frame(x = data[, x], y = data[, y])
+  
+  #######
+  # DENSITY FUNCTION
+  #######
+  get_density <- function(x, y, ...) {
+    dens <- MASS::kde2d(x, y, ...)
+    ix <- findInterval(x, dens$x)
+    iy <- findInterval(y, dens$y)
+    ii <- cbind(ix, iy)
+    return(dens$z[ii])
+  }
+  
+  dat$density <- get_density(dat$x, dat$y, h = c(2, 2), n = 1000) 
+  
+  plot <- plot + geom_point(data = dat, aes(-x, -y), 
+                            size = 2.25, alpha = 0.85,
+                            colour = "#cc67ff") 
+  
+  ########
+  # ADD ARROWS 
+  ########
+  datapc <- data.frame(PC$L) 
+  mult <- min(
+    (max(data[, y]) - min(data[, y])) / (max(datapc[, y]) - min(datapc[, y])),
+    (max(data[, x]) - min(data[, x])) / (max(datapc[, x]) - min(datapc[, x]))
+  )
+  
+  datapc <- transform(datapc,
+                      v1 = 0.65 * mult * get(x),
+                      v2 = 0.65 * mult * get(y)
+  )
+  
+  plot <- plot + geom_segment(data = datapc, linejoin = "round", lineend = "round", 
+                              aes(x = 0, y = 0, xend = -v1, yend = -v2), size = 1.2, 
+                              arrow = arrow(length = unit(0.5, "cm")), alpha = 1, color = "black")
+  
+  # Percentage for the axes
+  percentage <- round(diag(PC$Eval) / sum(PC$Eval) * 100, 2) 
+  
+  plot <- plot + geom_segment(data = datapc, aes(x = 0, y = 0, xend = -v1, yend = -v2), 
+                              size = 0.8, arrow = arrow(length = unit(0, "cm")), 
+                              linetype = 2, alpha = 0.8, color = "black")
+  
+  rownames(PC$L) <- c("S", "FN", "FS", "SL", "ON", "PH", "FL", "FL-LS")
+  PCAloadings <- data.frame(Variables = rownames(PC$L), PC$L)
+  
+  # Position labels at the end of segments
+  label_positions <- -datapc[, c("v1", "v2")]
+  
+  plot <- plot + annotate("text", x = label_positions[[1]]*c(1.15,1.2,1.1,1.18,1,1.15,1,1.5), y = label_positions[[2]]*c(1.8,1.35,1,0.9,1,1.5,1.8,-13), 
+                          label = PCAloadings$Variables, color = "black", 
+                          size = 6, fontface = 2, vjust = 1.5)  # Adjust vjust if needed
+  
+  # CHANGE THEME 
+  plot <- plot + theme_ms() + 
+    ylim(-4, 4) + 
+    xlim(-4, 4) + 
+    scale_y_reverse() +  # This reverses the y-axis
+    theme(legend.position = c(0.220, 0.130)) + 
+    xlab("PC1") + 
+    ylab("PC2")
+  
+  plot + theme(legend.position = "top", legend.justification = c(0, 1))
+}
+# Call the function
+local_phenobs_pca(PC)
+
+
+
+
