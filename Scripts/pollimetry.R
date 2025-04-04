57cd2c831d8c2502a78f448592f623d58e82d18e
Jose B. Lanuza <barragansljose@gmail.com>
2025-04-04 13:52:42 +0200

    edits

--- Scripts/pollimetry.R
+++ Scripts/pollimetry.R
@@ -5,30 +5,29 @@
 #Load library
 library(pollimetry)
 library(pollimetrydata)
+library(dplyr)
 
 #Load pollinator trait data
 polltraits = readRDS("Data/Trait_data/Processed/PollTraits.rds")
 
+#Read data
+matched_gbif_pollinators = readRDS("Data/Working_files/matched_gbif_pollinators.rds")
+
+#Join data
+d = left_join(polltraits, matched_gbif_pollinators)
+
+#Select cols of interest and rename to extrapolate tongue length
+colnames(d)
+#This is only implemented for these families
+bee_fam = c("Andrenidae", "Apidae", "Colletidae", "Halictidae", "Megachilidae")
+
+d1 = d %>% 
+select(c(IT_mm, Pollinator_family)) %>% 
+rename(Family = Pollinator_family) %>% 
+rename(IT = IT_mm) %>% 
+filter(Family %in% bee_fam)
 
 
+tongue_length = tonguelength(d1, mouthpart="all")
 
-(example <- cbind.data.frame(IT = c(1.2, 2.3), 
-                             Sex = c("Female","Male"), 
-                             Family = c("Apidae","Andrenidae"),
-                             Region = c("NorthAmerica","Europe"),
-                             Species = c("Ceratina_dupla","Andrena_flavipes")))
-
-
-example=cbind.data.frame(IT=c(1.3,2.3),
-                         Species = c("Ceratina_dupla","Andrena_flavipes"))
-
-
-
-
-tonguelength(example,mouthpart="all")
-
-bodysize(x = example, taxa = "bee", type = "taxo")
-?bodysize
-
-foragedist(c(10,5,2), type = "GreenleafAll") 
 
