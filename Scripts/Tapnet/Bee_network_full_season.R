853ad1e97fc30b294f5535135ffff05f2b34c1b6
Jose B. Lanuza <joseb.lanuza@iMac-de-Jose.local>
2025-07-03 11:45:22 +0200

    edits

---
+++ Scripts/Tapnet/Bee_network_full_season.R
@@ -0,0 +1,132 @@
+# ======================================================
+#Script: Prepare plant-poll networks (visits and visitation rate)
+# ======================================================
+
+#1)Total interactions
+#AND
+#2)Interaction frequency
+
+#Load libraries
+library(readr)
+library(tidyr)
+library(dplyr)
+library(purrr)
+library(stringr)
+library(tibble)
+# ======================================================
+# Load data
+# Interaction data
+raw_data = readRDS("Data/Working_files/interaction_data.rds")
+
+# Morphometrics to get phenobs species vector
+morphometrics = read_csv("Data/Trait_data/Raw/ReproductiveTraits_Morphometrics.csv")
+colnames(morphometrics)
+phenobs_spp = morphometrics %>% 
+  select(Species) %>% 
+  mutate(Species = str_replace(Species, "Persicaria bistorta", "Polygonum bistorta")) %>% 
+  mutate(Species = str_replace(Species, "Aquilegia chrysantha", "Aquilegia vulgaris")) %>% 
+  distinct() %>% 
+  pull(Species) 
+# ======================================================
+# Create vector of mail orders
+bee_fam = c("Megachilidae", 
+            "Apidae",
+            "Colletidae",
+            "Andrenidae",
+            "Halictidae",
+            "Mellittidae")
+
+# Prepare interaction data
+interaction_data = raw_data %>%
+  filter(!is.na(Interactions),
+         !is.na(Floral_abundance),
+         Pollinator != "None") %>%
+  rename(Plants = Plant_accepted_name,
+         Pollinators = Pollinator_accepted_name) %>%
+  filter(!is.na(Pollinators)) %>% 
+  filter(Pollinator_rank == "SPECIES") %>% 
+  filter(Sampling == "Focal") %>% 
+  # filter(!Pollinators == "Apis mellifera") %>% 
+  filter(Pollinator_family %in% bee_fam) %>% 
+  filter(!Plants == "Iberis sempervirens") 
+
+interaction_data = interaction_data %>% 
+  filter(Plant %in% phenobs_spp) 
+
+# ======================================================
+#1)Total interactions
+
+#Convert to network
+to_network = function(data) {
+  data %>%
+    group_by(Plants, Pollinators) %>%
+    summarise(Total_interactions = sum(Interactions),
+              .groups = "drop") %>%
+    pivot_wider(names_from = Pollinators, 
+                values_from = Total_interactions, values_fill = 0) %>%
+    column_to_rownames("Plants") %>%
+    as.matrix()
+}
+
+#Prepare networks per garden
+networks_by_garden_interactions = interaction_data %>%
+  group_by(Botanical_garden) %>%
+  nest() %>%
+  mutate(Interaction_network = map(data, to_network)) %>%
+  select(!data) %>% 
+  ungroup()
+
+# ======================================================
+#2)Interaction frequency
+#Overwrite interactions with interaction frequency 
+#to minimise edits in code
+interaction_frequency = interaction_data %>% 
+  group_by(Botanical_garden, Plants, Pollinators) %>%
+  summarise(Interaction_total = sum(Interactions))
+
+total_time_species = interaction_data %>%
+  select(Botanical_garden, Plants, Date, Total_time_species) %>% 
+  group_by(Botanical_garden, Plants) %>%
+  distinct() %>% 
+  summarise(Total_time_species = sum(Total_time_species, na.rm = TRUE), .groups = "drop")
+
+# Join total time back into interaction data
+interaction_data_freq = interaction_frequency %>%
+  left_join(total_time_species, by = c("Botanical_garden", "Plants"), suffix = c("", "_summed")) %>%
+  mutate(Freq = Interaction_total / Total_time_species)
+
+
+# Now convert to interaction frequency network
+to_freq_network = function(data) {
+  data %>%
+    group_by(Plants, Pollinators) %>%
+    summarise(Total_frequency = sum(Freq), .groups = "drop") %>%
+    pivot_wider(names_from = Pollinators, 
+                values_from = Total_frequency, values_fill = 0) %>%
+    column_to_rownames("Plants") %>%
+    as.matrix()
+}
+
+# Prepare networks per garden (frequencies)
+networks_by_garden_int_frequency = interaction_data_freq %>%
+  group_by(Botanical_garden) %>%
+  nest() %>%
+  mutate(Int_frequency_network = map(data, to_freq_network)) %>%
+  select(!data) %>%
+  ungroup()
+
+
+# Now bind both and get a tibble with total int and int freq
+net_by_garden = left_join(networks_by_garden_interactions,
+                          networks_by_garden_int_frequency)
+
+
+# ======================================================
+# Save data
+saveRDS(net_by_garden, "Data/Working_files/networks_by_garden_only_phenobs_bees.rds")
+
+
+#Safety check
+cor(c(dist(net_by_garden$Interaction_network[[1]])),
+    c(dist(net_by_garden$Int_frequency_network[[1]])))
+
---
+++ Scripts/Tapnet/Bee_phylo.R
@@ -0,0 +1,104 @@
+# Phylogenetic data:
+# Henríquez-Piskulich, P.; Hugall, A.F.; Stuart-Fox; D. 2024.
+# A supermatrix phylogeny of the world’s bees (Hymenoptera: Anthophila). 
+# Molecular Phylogenetics and Evolution 190, 107963. doi:10.1016/j.ympev.2023.107963.
+
+# Load library
+library(ape)
+library(phytools)
+# Read tree
+tree = read.tree("Data/Working_files/BEE_mat7_IQTufbs_tplo_1001bin.nwk")
+# Extract just the first one (ML)
+ml_tree <- tree[[1]]
+
+# Prepare bee species from data
+# Load data
+# Interaction data
+raw_data = readRDS("Data/Working_files/interaction_data.rds")
+# Phenobs spp
+phenobs_spp = readRDS("Data/Working_files/phenobs_spp.rds")
+
+# Create vector of main bee families
+bee_fam = c("Megachilidae", 
+            "Apidae",
+            "Colletidae",
+            "Andrenidae",
+            "Halictidae",
+            "Mellittidae")
+
+# ======================================================
+# Prepare interaction data
+interaction_data = raw_data %>%
+  filter(!is.na(Interactions),
+         !is.na(Floral_abundance),
+         Pollinator != "None") %>%
+  rename(Plants = Plant_accepted_name,
+         Pollinators = Pollinator_accepted_name) %>%
+  filter(!is.na(Pollinators)) %>% 
+  filter(Pollinator_rank == "SPECIES") %>% 
+  filter(Sampling == "Focal") %>% 
+  # filter(!Pollinators == "Apis mellifera") %>% 
+  filter(Pollinator_family %in% bee_fam) %>% 
+  filter(!Plants == "Iberis sempervirens") 
+
+interaction_data = interaction_data %>% 
+  filter(Plant %in% phenobs_spp) 
+
+# Second, get phylogenetic information for pollinators
+poll_taxonomic_info = interaction_data %>% 
+  select(c(Pollinators, Pollinator_genus, Pollinator_family, Pollinator_order)) %>% 
+  distinct() %>% 
+  rename(species = Pollinators,
+         genus = Pollinator_genus,
+         family = Pollinator_family,
+         order = Pollinator_order) %>% 
+  select(order, family, genus, species)
+
+# Prepare species with underscore to match phylo
+poll_taxonomic_info$full_name = str_replace(poll_taxonomic_info$species, " ", "_")
+# Get tip labels
+tree_tips = ml_tree$tip.label
+# Find which species from your list are in the tree
+matching_species = poll_taxonomic_info$full_name[poll_taxonomic_info$full_name %in% tree_tips]
+# Unmatched species
+unmatching_species = poll_taxonomic_info$full_name[!poll_taxonomic_info$full_name %in% tree_tips]
+# Keep tip
+pruned_tree = keep.tip(ml_tree, matching_species)
+# Plot
+ggtree(pruned_tree, size=0.1, open.angle=5, alpha=0.5) +
+  geom_tippoint(colour='cyan4') +
+  geom_tiplab(linetype='dashed', linesize=.05, 
+              size=1.75, color= "black", offset = 0.2, fontface=2) +
+  theme(plot.margin = margin(5, 50, 5, 5)) +
+  coord_cartesian(clip = "off")
+
+# I need to add only a single species, at the moment is being added like this
+## Function for adding a cherry to a tree where a single tip was before
+add.cherry <- function(tree, tip, new.tips) {
+  
+  ## Find the edge leading to the tip
+  tip_id <- match(tip, tree$tip.label)
+  
+  ## Create the new cherry
+  tree_to_add <- ape::stree(length(c(tip, new.tips)))
+  
+  ## Naming the tips
+  tree_to_add$tip.label <- c(tip, new.tips)
+  
+  ## Add 0 branch length
+  tree_to_add$edge.length <- rep(0, Nedge(tree_to_add))
+  
+  ## Binding both trees
+  return(bind.tree(tree, tree_to_add, where = tip_id))
+}
+
+## Adding a new sister taxon to t6 (with a 0 branch length)
+new_tree = add.cherry(pruned_tree, tip = "Andrena_pilipes", new.tips = unmatching_species)
+ggtree(new_tree, size=0.1, open.angle=5, alpha=0.5) +
+  geom_tippoint(colour='cyan4') +
+  geom_tiplab(linetype='dashed', linesize=.05, 
+              size=1.75, color= "black", offset = 0.2, fontface=2) +
+  theme(plot.margin = margin(5, 50, 5, 5)) +
+  coord_cartesian(clip = "off")
+
+saveRDS(new_tree, "Data/Working_files/bee_phylo.rds")
