5aebb4dfacf2f632cebcb06c328b33ca5e52bbe6
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2025-07-04 12:54:38 +0200

    Update test_tapnet_full_season.R

--- Scripts/Tapnet/test_tapnet_full_season.R
+++ Scripts/Tapnet/test_tapnet_full_season.R
@@ -7,7 +7,7 @@ library(tapnet)
 library(ape)
 library(dplyr)
 library(stringi)
-
+library(readxl)
 # ======================================================
 # Load data
 # Load philo--------------------------------------------
@@ -38,83 +38,110 @@ poll_ab_full_season_by_sp <- readr::read_csv("Data/Working_files/total_poll_abun
 
 gardens <- net_by_garden_full_season$Botanical_garden
 
-# Leipzing---------------------------------------------
-garden_number <- 3
-garden_name <- gardens[garden_number]
-garden_name
-# Interaction network
-garden_full_season_net <- net_by_garden_full_season$Interaction_network[garden_number]
-poll_garden_full_season_net_names <- colnames(garden_full_season_net[[1]])
-plant_garden_full_season_net_names <- rownames(garden_full_season_net[[1]])
+for (garden_number in 1:length(gardens)) {
+  
+  # Garden---------------------------------------------
+  garden_name <- gardens[garden_number]
+  garden_name
+  # Interaction network
+  garden_full_season_net <- net_by_garden_full_season$Interaction_network[garden_number]
+  poll_garden_full_season_net_names <- colnames(garden_full_season_net[[1]])
+  plant_garden_full_season_net_names <- rownames(garden_full_season_net[[1]])
+  
+  # Phylo
+  poll_philo_names <- poll_phylo$tip.label
+  plant_philo_names <- plant_phylo$tip.label
+  
+  # Sanity check
+  poll_garden_full_season_net_names[!poll_garden_full_season_net_names %in% poll_philo_names]
+  plant_garden_full_season_net_names[!plant_garden_full_season_net_names %in% plant_philo_names] # OK
+  # pollinatir sp names need an underscore
+  
+  # Modify poll names
+  colnames(garden_full_season_net[[1]]) <- gsub(" ", "_",poll_garden_full_season_net_names)
+  # Sanity check
+  poll_garden_full_season_net_names <- colnames(garden_full_season_net[[1]])
+  poll_garden_full_season_net_names[!poll_garden_full_season_net_names %in% poll_philo_names] # OK
+  
+  # Abundances
+  floral_ab_garden_full_season_raw <- floral_ab_full_season_by_sp %>% 
+    filter(Botanical_garden == garden_name)
+  poll_ab_garden_full_season_raw <- poll_ab_full_season_by_sp  %>% 
+    filter(Botanical_garden == garden_name)
+  
+  # Replace sp name by accepted sp name
+  plant_names_raw_data <-  readRDS("Data/Working_files/interaction_data.rds") %>%
+    dplyr::select(Plant,Plant_accepted_name) %>% unique() %>% sort()
+  poll_names_raw_data <-  readRDS("Data/Working_files/interaction_data.rds") %>%
+    dplyr::select(Pollinator, Pollinator_accepted_name) %>% unique() %>% sort()
+  
+  floral_ab_garden_full_season <- floral_ab_garden_full_season_raw %>%
+    left_join(plant_names_raw_data, by = "Plant") %>% 
+    rename(Old_Plant = Plant, 
+           Plant = Plant_accepted_name) %>%
+    filter(Plant %in% rownames(garden_full_season_net[[1]]))
+  
+  poll_ab_garden_full_season <- poll_ab_garden_full_season_raw %>%
+    left_join(poll_names_raw_data, by = "Pollinator") %>% 
+    rename(Old_Pollinator = Pollinator, Pollinator = Pollinator_accepted_name) %>%
+    mutate(Pollinator = gsub(" ", "_", Pollinator)) %>%
+    filter(Pollinator %in% colnames(garden_full_season_net[[1]]))
+  
+  # Sanity check
+  floral_ab_garden_full_season %>% filter(is.na(Plant))
+  poll_ab_garden_full_season %>% filter(is.na(Pollinator))
+  
+  # Fix pollinator names
+  poll_ab_garden_full_season_names <-  gsub(" ", "_",poll_ab_garden_full_season$Pollinator)
+  
+  # Sanity check
+  plant_garden_full_season_net_names[!plant_garden_full_season_net_names %in%  
+                                       floral_ab_garden_full_season$Plant]
+  
+  poll_garden_full_season_net_names[!poll_garden_full_season_net_names %in%  
+                                      poll_ab_garden_full_season_names]
+  
+  plant_abun_garden_full_season_vector <- setNames(
+    floral_ab_garden_full_season$Total_floral_abundance,
+    floral_ab_garden_full_season$Plant
+  )
+  
+  poll_abun_garden_full_season_vector <- setNames(
+    poll_ab_garden_full_season$Total_pollinator_abundance,
+    gsub(" ", "_",poll_ab_garden_full_season$Pollinator)
+  )
+  
+  
+  tapnet_web1 <- make_tapnet(tree_low = plant_phylo, tree_high = poll_phylo,
+                             networks = garden_full_season_net[[1]], traits_low = NULL,
+                             traits_high = NULL, abun_low = plant_abun_garden_full_season_vector,
+                             abun_high = poll_abun_garden_full_season_vector, npems_lat = 4)
+  
+  
+  # str(tapnet_web1) # show tapnet structure
+  # 
+  # colnames(tapnet_web1$networks[[1]]$pems$low) # names of fitted PEMs                                                                                                                                                                                            are required for prediction from web1 to web 2). Let’s check:
+  # colnames(tapnet_web1$networks[[1]]$pems$high)
+  # 
+  # # check for correlation between the phylogenetic eigenvectors and the observed traits:
+  # cor(cbind(tapnet_web1$networks[[1]]$pems$low, tapnet_web1$networks[[1]]$traits$low))
+  # cor(cbind(tapnet_web1$networks[[1]]$pems$high, tapnet_web1$networks[[1]]$traits$high))
+  
+  # We here assume that all trait matches are best described using a normal distribution. Alternatively, we could
+  # use the shifted log-normal.
+  
+  fit_web1 <- fit_tapnet(tapnet = tapnet_web1, method="SANN")
+  
+  # goodness of fit
+  gof_web1_norm <- gof_tapnet(fit_web1)
+  
+  # save info
+  name_fit <- paste0("Data/Working_files/",garden_name,"fit_TAPNET_full_season_NO_traits.rds")
+  name_gof <- paste0("Data/Working_files/",garden_name,"gof_TAPNET_full_season_NO_traits.rds")
+  
+  saveRDS(fit_web1, name_gof)
+  saveRDS(gof_web1_norm, name_gof)
+  
+}
 
-# Phylo
-poll_philo_names <- poll_phylo$tip.label
-plant_philo_names <- plant_phylo$tip.label
 
-# Sanity check
-poll_garden_full_season_net_names[!poll_garden_full_season_net_names %in% poll_philo_names]
-plant_garden_full_season_net_names[!plant_garden_full_season_net_names %in% plant_philo_names] # OK
-# pollinatir sp names need an underscore
-
-# Modify poll names
-colnames(garden_full_season_net[[1]]) <- gsub(" ", "_",poll_garden_full_season_net_names)
-# Sanity check
-poll_garden_full_season_net_names <- colnames(garden_full_season_net[[1]])
-poll_garden_full_season_net_names[!poll_garden_full_season_net_names %in% poll_philo_names] # OK
-
-# Abundances
-floral_ab_garden_full_season <- floral_ab_full_season_by_sp %>% 
-  filter(Botanical_garden == garden_name)
-poll_ab_garden_full_season <- poll_ab_full_season_by_sp  %>% 
-  filter(Botanical_garden == garden_name)
-
-poll_ab_garden_full_season_names <-  gsub(" ", "_",poll_ab_garden_full_season$Pollinator)
-
-floral_ab_garden_full_season$Plant = stringr::str_replace(floral_ab_garden_full_season$Plant, "Anemone nemorosa", "Anemonoides nemorosa")
-floral_ab_garden_full_season$Plant = str_replace(floral_ab_garden_full_season$Plant, "Anemone sylvestris", "Anemonoides sylvestris")
-floral_ab_garden_full_season$Plant = str_replace(floral_ab_garden_full_season$Plant, "Eriocapitella hupehensis", "Anemone hupehensis")
-floral_ab_garden_full_season$Plant = str_replace(floral_ab_garden_full_season$Plant, "Penstemon bradburyi", "Penstemon grandiflorus")
-floral_ab_garden_full_season$Plant = str_replace(floral_ab_garden_full_season$Plant, "Viscaria vulgaris", "Silene viscaria")
-
-
-
-# Sanity check
-plant_garden_full_season_net_names[!plant_garden_full_season_net_names %in%  
-                                     floral_ab_garden_full_season$Plant]
-
-poll_garden_full_season_net_names[!poll_garden_full_season_net_names %in%  
-                                    poll_ab_garden_full_season_names]
-
-plant_abun_garden_full_season_vector <- setNames(
-  floral_ab_garden_full_season$Total_floral_abundance,
-  gsub(" ", "_",floral_ab_garden_full_season$Plant)
-)
-
-poll_abun_garden_full_season_vector <- setNames(
-  poll_ab_garden_full_season$Total_pollinator_abundance,
-  gsub(" ", "_",poll_ab_garden_full_season$Pollinator)
-)
-
-
-tapnet_web1 <- make_tapnet(tree_low = plant_phylo, tree_high = poll_phylo,
-                           networks = garden_full_season_net[[1]], traits_low = NULL,
-                           traits_high = NULL, abun_low = plant_abun_garden_full_season_vector,
-                           abun_high = poll_abun_garden_full_season_vector, npems_lat = 4)
-
-str(tapnet_web1) # show tapnet structure
-
-colnames(tapnet_web1$networks[[1]]$pems$low) # names of fitted PEMs                                                                                                                                                                                            are required for prediction from web1 to web 2). Let’s check:
-colnames(tapnet_web1$networks[[1]]$pems$high)
-
-# check for correlation between the phylogenetic eigenvectors and the observed traits:
-cor(cbind(tapnet_web1$networks[[1]]$pems$low, tapnet_web1$networks[[1]]$traits$low))
-cor(cbind(tapnet_web1$networks[[1]]$pems$high, tapnet_web1$networks[[1]]$traits$high))
-
-# We here assume that all trait matches are best described using a normal distribution. Alternatively, we could
-# use the shifted log-normal.
-
-fit_web1 <- fit_tapnet(tapnet = tapnet_web1, method="SANN")
-
-# goodness of fit
-gof_web1_norm <- gof_tapnet(fit_web1)
-gof_web1_norm
