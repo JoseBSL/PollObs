57fefe6c445125ec3e21f4f11cebcdcd577eea98
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2025-07-16 18:29:37 +0200

    update metrics for EARLY, MID and LATE season TAPNET

--- Scripts/Tapnet/extract_early_mid_late_season_results_TAPNET.R
+++ Scripts/Tapnet/extract_early_mid_late_season_results_TAPNET.R
@@ -92,13 +92,13 @@ for (garden_number in 1:length(gardens)) {
       
       
       # Store garden-season results
-      aux_names <- c("delta", "tmatch_width_pem", "tmatch_width_obs", 
+      aux_names <- c("delta", "tmatch_width_pem", "tmatch_width_obs", "bc_sim_web", "cor_web",
                      "rho_PEM_abundance_plant", "rho_pvalue_PEM_abundance_plant", 
                      "rho_PEM_abundance_poll", "rho_pvalue_PEM_abundance_poll", 
                      "rho_PEM_traits_plant", "rho_pvalue_PEM_traits_plant", 
                      "rho_PEM_traits_poll", "rho_pvalue_PEM_traits_poll")
       
-      aux_values <- c( delta, tmatch_width_pem, tmatch_width_obs, 
+      aux_values <- c( delta, tmatch_width_pem, tmatch_width_obs, bc_sim_web, cor_web,
                        rho_PEM_abundance_plant, rho_pvalue_PEM_abundance_plant, 
                        rho_PEM_abundance_poll, rho_pvalue_PEM_abundance_poll, 
                        rho_PEM_traits_plant, rho_pvalue_PEM_traits_plant, 
