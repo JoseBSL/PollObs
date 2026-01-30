2d3bd6b3c892fedea492380fd52a999d3b53cd04
Jose B. Lanuza <joseb.lanuza@MacBook-Pro-de-Jose.local>
2026-01-31 00:45:44 +0100

    edits

--- Scripts/5_3_Networks_by_week/Sizematching_graph_mantel.R
+++ Scripts/5_3_Networks_by_week/Sizematching_graph_mantel.R
@@ -39,7 +39,7 @@ pheno_season_dot <- pheno_season %>%
 # --- Outside facet labels ---
 panel_labels <- data.frame(
   Botanical_garden = c("Halle", "Jena", "Leipzig"),
-  label = c("d) Halle", "e) Jena", "f) Leipzig"),
+  label = c("g) Halle", "h) Jena", "i) Leipzig"),
   x = -Inf, y = Inf
 )
 
@@ -50,7 +50,7 @@ dummy_weekly <- pheno_week_box %>%
   slice(1)
 
 # --- Plot ---
-panel2 <- ggplot(pheno_week_box, aes(x = Season, y = Mantel_corr, fill = Season)) +
+panel3 <- ggplot(pheno_week_box, aes(x = Season, y = Mantel_corr, fill = Season)) +
   
   geom_violin(alpha = 0.45, width = 0.65, colour = NA) +
   
