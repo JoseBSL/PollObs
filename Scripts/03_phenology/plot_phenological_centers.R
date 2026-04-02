64fae1689d3eaf47bb8ced4a8088430b20320d09
Jose B. Lanuza <joseb.lanuza@MacBook-Pro-de-Jose.local>
2026-04-02 19:00:39 +0200

    ms

--- Scripts/03_phenology/plot_phenological_centers.R
+++ Scripts/03_phenology/plot_phenological_centers.R
@@ -4,49 +4,9 @@ library(ggdist)
 
 # Read processed data
 pollinator_centers <- readRDS("Data/Working_files/pollinator_pheno_centers.rds")
-plant_centers      <- readRDS("Data/Working_files/plant_pheno_centers.rds")
+plant_centers <- readRDS("Data/Working_files/plant_pheno_centers.rds")
 
-#--------------------------------------------------
-# Combine plant (by garden) + pollinator centers
-#--------------------------------------------------
-
-plot_data <- bind_rows(
-  plant_centers %>%
-    transmute(Group = paste(Garden, "plants"), center),
-  pollinator_centers %>%
-    transmute(Group = "Pollinators", center)
-) %>%
-  mutate(
-    Group = factor(
-      Group,
-      levels = c("Pollinators", "Jena plants", "Halle plants", "Leipzig plants")
-    )
-  )
-
-#--------------------------------------------------
-# Compute summaries (median)
-#--------------------------------------------------
-
-plot_summary <- plot_data %>%
-  group_by(Group) %>%
-  summarise(
-    q50 = quantile(center, 0.50, na.rm = TRUE),
-    .groups = "drop"
-  )
-
-#--------------------------------------------------
-# Plot
-#--------------------------------------------------
-
-library(dplyr)
-library(ggplot2)
-library(ggdist)
-
-# Read processed data
-pollinator_centers <- readRDS("Data/Working_files/pollinator_pheno_centers.rds")
-plant_centers      <- readRDS("Data/Working_files/plant_pheno_centers.rds")
-
-# Combine data
+# Combine plant (by garden) and pollinator centres
 plot_data <- bind_rows(
   plant_centers %>%
     transmute(Group = paste(Garden, "plants"), center),
@@ -64,21 +24,27 @@ plot_data <- bind_rows(
 plot_summary <- plot_data %>%
   group_by(Group) %>%
   summarise(
-    q50 = quantile(center, 0.50, na.rm = TRUE),
+    q50 = median(center, na.rm = TRUE),
     .groups = "drop"
   )
 
-# Define colors
+# Define colours
 my_colors <- c(
-  "Pollinators"   = "grey70",
-  "Jena plants"   = "#B12A90FF",
-  "Halle plants"  = "#0D0887FF",
-  "Leipzig plants"= "#FCA636FF"
+  "Pollinators"    = "grey70",
+  "Jena plants"    = "#B12A90FF",
+  "Halle plants"   = "#0D0887FF",
+  "Leipzig plants" = "#FCA636FF"
 )
 
 # Plot
 ggplot(plot_data, aes(x = Group, y = center, fill = Group)) +
-  ggdist::stat_halfeye(
+  
+  geom_point(
+    shape = 95,
+    size = 6,
+    alpha = 0.85
+  )+
+  stat_halfeye(
     adjust = 0.5,
     width = 0.6,
     .width = 0,
@@ -90,18 +56,13 @@ ggplot(plot_data, aes(x = Group, y = center, fill = Group)) +
     width = 0.15,
     outlier.shape = NA,
     alpha = 0.6
-  ) +
-  geom_point(
-    shape = 95,
-    size = 6,
-    alpha = 0.2
-  ) +
+  )  +
   geom_point(
     data = plot_summary,
     aes(x = Group, y = q50),
     inherit.aes = FALSE,
     size = 3,
-    color = "black"
+    colour = "black"
   ) +
   scale_fill_manual(values = my_colors) +
   coord_cartesian(clip = "off") +
@@ -110,5 +71,4 @@ ggplot(plot_data, aes(x = Group, y = center, fill = Group)) +
     legend.position = "none",
     axis.title.x = element_blank()
   ) +
-  labs(
-    y = "Phenological center (DOY)")
+  labs(y = "Phenological center (DOY)")
